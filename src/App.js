cfbb3f577ffe72cef6394ee7930b881eda65a76a
Abhay-codes <abhaysingh002001@gmail.com>
2023-10-20 02:49:58 +0530

    function to find winner added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import './style.css';
+function Winner(square){
+  const pattern =[
+    [0,1,2],
+    [3,4,5],
+    [5,6,7],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6]
+
+
+  ];
+  for (let i=0;i<pattern.length;i++){
+    const[a,b,c]=pattern[i];
+    if(square[a] && square[a]===square[b]&&square[b]===square[c]){
+      return square[a];
+    }
+  }
+  return null;
+
+
+
+ }
 
 function Square({val,onSquareClick}){
 
@@ -16,6 +40,7 @@ function Square({val,onSquareClick}){
 function Board () {
   const [squares,setsquare]=useState(Array(9).fill(null));
   const[isnextx,setnext]=useState(true);
+  
   //let nextx=false;
   function handleClick(i){
 
@@ -36,8 +61,20 @@ function Board () {
     setnext(!isnextx);
     setsquare(nextsquare);
   }
+  const winner =Winner(squares);
+  let status ;
+  if (winner){
+    status ="winner is:"+winner;
+  }
+  else{
+    status ="next move is for: " + (isnextx?"x":"O");
+  }
+
+
   return (
     <>
+    <div>
+    
       <div className= "boardrow">
         <Square val ={squares[0]} onSquareClick={()=>handleClick(0)}/>
         <Square val ={squares[1]} onSquareClick={()=>handleClick(1)}/>
@@ -60,7 +97,12 @@ function Board () {
         
       </div>
 
-
+      </div>
+      <div>
+        <p>
+          {status}
+        </p>
+      </div>
     </>
   );
 }
