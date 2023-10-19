92d55bb9e69e003f4608f9a60b6184c5efdac3ed
Abhay-codes <abhaysingh002001@gmail.com>
2023-10-20 01:20:32 +0530

    moved state to board, passing value as props to squares

--- src/App.js
+++ src/App.js
@@ -3,42 +3,45 @@ import './App.css';
 import { useState } from 'react';
 import './style.css';
 
-function Square(){
-  const [value,setvalue]=useState(null);
-  function handleclick(){
-    setvalue('X')
-  }
+function Square({val,onSquareClick}){
+
   return(
-    <button className="square" onClick={handleclick} >
-    {value} 
+    <button className="square" onClick={onSquareClick} >
+    {val} 
      
 
     </button>
   )
 }
 function Board () {
+  const [squares,setsquare]=useState(Array(9).fill(null));
+  function handleClick(i){
+
+    const nextsquare=squares.slice();
+    nextsquare[i]='X';
+    setsquare(nextsquare);
+  }
   return (
     <>
       <div className= "boardrow">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square val ={squares[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square val ={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square val ={squares[2]} onSquareClick={()=>handleClick(2)}/>
 
         
       </div>
       <div className= "boardrow">
-        <Square/>
-        <Square/>
-        <Square/>
+      <Square val ={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square val ={squares[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square val ={squares[5]} onSquareClick={()=>handleClick(5)}/>
 
         
       </div>
 
       <div className= "boardrow">
-        <Square/>
-        <Square/>
-        <Square/>
-
+      <Square val ={squares[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square val ={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square val ={squares[8]} onSquareClick={()=>handleClick(8)}/>
         
       </div>
 
